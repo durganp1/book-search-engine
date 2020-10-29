792608d674a394334806ab85fd40232031babb97
durganp1 <durganp1@gmail.com>
2020-10-29 14:00:49 -0500

    installed apollo server

---
+++ Develop/server/schemas/typeDefs.js
@@ -0,0 +1,11 @@
+
+
+// import the gql tagged template function
+const {gql} = require('apollo-server-express');
+
+// create typeDefs
+const typeDefs = gql``;
+
+
+// export the typeDefs
+module.exports = typeDefs;
